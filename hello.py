76ec0a96fca3efe2a999a3582a3a67441fbafec9
yoni053 <111363275+yoni053@users.noreply.github.com>
2022-12-21 22:14:12 +0900

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,15 @@
+#크롤링
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20210829',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+#old_content > table > tbody > tr:nth-child(4) > td.title > div > a
+trs = soup.select('#old_content > table > tbody > tr')
+for tr in trs:
+    a = tr.select_one('td.title > div > a')
+    if a is not None:
+        print(a.text)
